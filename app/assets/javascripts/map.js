1524b40e44a6fa411048a37f0b3f5e39b5e9411d
wbvg-mac <will@mylittleshowpony.com>
2013-07-24 23:05:03 +1000

    piecing google style maps

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,5 +1,9 @@
   var map;
   var canvas;
+  var willywoo = 'custom_style';
+  var google;
+
+
 
   var display_map = function (lat, long, zoom) {
     canvas = $('#map_canvas')[0];
@@ -7,20 +11,68 @@
     if (! canvas)
       return; // I QUIT
 
+    function initialize() {
+       var featureOpts = [
+        {
+          stylers: [
+            { hue: '#ffaa00' },
+            { weight: 3.8 }
+          ]
+        },{
+          elementType: "labels.icon",
+          stylers: [
+            { visibility: "on" },
+            { invert_lightness: true },
+            { color: "#f90580" }
+          ]
+        },{
+          elementType: "labels.text.fill",
+          stylers: [
+            { "color": "#002f80" }
+          ]
+        },{
+        }
+      ];
+
+
       var mapOptions = {
       center: new google.maps.LatLng(lat, long),
       zoom: zoom,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+      mapTypeControlOptions: {
+      mapTypeId: [google.maps.MapTypeId.ROADMAP, willywoo]
+       },
 
-      map = new google.maps.Map(canvas, mapOptions);
-    };
+       mapTypeId: willywoo
+     };
+
+//fucking around w/ styling
+  map = new google.maps.Map(canvas, mapOptions);
+
+
+      var styledMapOptions = {
+        name: 'Custom Style'
+      };
+
+
+  var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+
+  map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+
+
+  };
+};
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+
 
   var add_marker = function (lat, long, name, icon) {
 
     var latlng = new google.maps.LatLng(lat, long);
     var marker = new google.maps.Marker({position: latlng, icon: image, map: map, name: name});
     var image = 'images/coffee.png';
+
+
   };
 
 
---
+++ app/assets/javascripts/map_working.js
@@ -0,0 +1,36 @@
+  var map;
+  var canvas;
+
+  var display_map = function (lat, long, zoom) {
+    canvas = $('#map_canvas')[0];
+
+    if (! canvas)
+      return; // I QUIT
+
+      var mapOptions = {
+      center: new google.maps.LatLng(lat, long),
+      zoom: zoom,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+      map = new google.maps.Map(canvas, mapOptions);
+    };
+
+  var add_marker = function (lat, long, name, icon) {
+
+    var latlng = new google.maps.LatLng(lat, long);
+    var marker = new google.maps.Marker({position: latlng, icon: image, map: map, name: name});
+    var image = 'images/coffee.png';
+  };
+
+
+$(document).ready(function () {
+  display_map(-33.8942659, 151.2654561, 15);
+
+});
+
+
+
+
+
+
